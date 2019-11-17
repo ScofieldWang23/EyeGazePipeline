f5e40dd2db76e9a9fbe320c29263463f9f6b6c92
“sfwang” <scofieldwang618@gmail.com>
2019-11-17 17:09:49 -0500

    change get_training.py & evaluation.py

--- DataCollection/get_training.py
+++ DataCollection/get_training.py
@@ -4,33 +4,82 @@ from pandas import ExcelWriter
 from pandas import ExcelFile
 import shutil
 
-# excel directortyx-
-excel_directory = r'C:\Users\zhangzhida\Desktop\eyegaze\data.csv'
 
-# raw photo directory
-raw_photo_directory = r'C:\Users\zhangzhida\Desktop\eyegaze\raw_data'
+class AddLable():
+    def __init__(self, gaze_data_file_name, result_dir_name):
+        self.base_dir = os.getcwd()
+        self.result_dir = os.path.join(self.base_dir, result_dir_name)
+        self.num_of_cameras = 2
+        self.gaze_data = pd.read_csv(os.path.join(self.base_dir, self.gaze_data_file_name))
+        print("The shape of gaze_data.csv: ".format(self.gaze_data.shape))
 
-# result photo directory
-result_directory = r'C:\Users\zhangzhida\Desktop\eyegaze\preparation'
 
-# num of cameras
-num_of_cameras = 2
+    def __del__(self):
+        pass
 
-# read excel
-df = pd.read_csv(excel_directory)
 
-def move_to_result_directory(filename,source_directory, result_directory, sub_directory):
-    print("move_to")
-    # create directory if not exists
-    if not os.path.exists(result_directory):
-        os.makedirs(result_directory)
+    def move_to_result_directory(self, filename, source_dir, sub_dir):
+        print("move has been made!")
+        # create directory if not exists
+        if not os.path.exists(self.result_dir):
+            os.makedirs(self.result_dir)
+        
+        if not os.path.exists(os.path.join(self.result_dir, sub_dir)):
+            os.makedirs(os.path.join(self.result_dir, sub_dir))
+        
+        destination_file_name = os.path.join(self.result_dir, sub_dir, filename)
+        source_filename = os.path.join(source_dir, filename)
+        # os.rename(destination_file_name)
+        shutil.copy2(source_filename, destination_file_name)
+
+
+    def process_raw_directory(self, id_camera):
+        raw_photo_dir_i_cam = os.path.join(self.base_dir, 'face' + str(id_camera))
+        # iterate photo directory and .csv file
+        list_raw_photo_directory = os.listdir(raw_photo_dir_i_cam)
+        list_raw_photo_directory.sort()
+        delimeter = "_"
+        delay_threshold = 0.01
+
+        i = 0
+        j = 0 
+        while i < len(list_raw_photo_directory) and j < self.gaze_data.shape[0]:
+            raw_photo_name = list_raw_photo_directory[i]
+            gaze_record = self.gaze_data.iloc[j]
+            
+            gaze_point_timestamp = gaze_record['timestamp']
+            raw_photo_timestamp = float(raw_photo_name.split(delimeter)[-1][:-4])
+            if gaze_point_timestamp != gaze_point_timestamp:
+                j += 1
+
+            diff_time = gaze_point_timestamp - raw_photo_timestamp
+
+            # if valid, with delay smaller than threshold
+            if abs(diff_time) < delay_threshold:
+                # print(diff_time)
+                if gaze_record['FPOGX'] is None:
+                    j += 1
+                    continue
+                elif gaze_record['FPOGX'] < 0.5:
+                    self.move_to_result_directory(raw_photo_name, raw_photo_dir_i_cam, self.result_dir, r'left')
+                else:
+                    self.move_to_result_directory(raw_photo_name, raw_photo_dir_i_cam, self.result_dir, r'right')
+                    
+                i += 1
+                # print(idx_of_camera, gaze_point_timestamp, raw_photo_timestamp)     
+
+            if gaze_point_timestamp < raw_photo_timestamp:
+                j += 1
+            else:
+                i += 1
     
-    if not os.path.exists( os.path.join(result_directory, sub_directory)):
-        os.makedirs(os.path.join(result_directory, sub_directory))
-    
-    destination_file_name = os.path.join(result_directory, sub_directory, filename)
-    source_filename = os.path.join(source_directory, filename)
-#     os.rename(destination_file_name)
-    shutil.copy2(source_filename, destination_file_name)
+
+
+if __name__ == "__main__":
+    obj = AddLable('gaze_data.csv', 'preparation')
+    for i in range(obj.num_of_cameras):
+        obj.process_raw_directory(i)
+    pass
+
 
     
--- pipeline/evaluation.py
+++ pipeline/evaluation.py
@@ -3,13 +3,104 @@ import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
 import shutil
-
-csv_filename = os.path.join(os.getcwd(), 'eval_prediction.csv')
-if not os.path.exists(csv_filename):
-    df = pd.DataFrame(columns=['img_name', 'img_time', 'gaze_time', 'real_label', 'predict_label'])
-    df.to_csv(csv_filename, mode='a', index=False)
-
-img_dir = r'C:\Users\zhangzhida\Desktop\EyeGazePipeline\data\screenshot'
-gaze_data_dir = r'C:\Users\zhangzhida\Desktop\EyeGazePipeline\observer'
+import matplotlib.pyplot as plt
 
 
+class evaluation():
+    def __init__(self, gaze_data_file_name, img_file_name, predict_file_name):
+        self.img_dir = r'C:\Users\zhangzhida\Desktop\EyeGazePipeline\data\screenshot'
+        self.gaze_data_dir = r'C:\Users\zhangzhida\Desktop\EyeGazePipeline\observer'
+        self.predict_dir = r'C:\Users\zhangzhida\Desktop\EyeGazePipeline\pipeline'
+        self.gaze_data_file_name = gaze_data_file_name
+        self.img_file_name = img_file_name
+        self.predict_file_name = predict_file_name
+
+        # csv_filename = os.path.join(os.getcwd(), 'eval_prediction.csv')
+        # if not os.path.exists(self.csv_filename):
+
+        self.eval_table = pd.DataFrame(columns=['img_name', 'img_time', 'gaze_time', 'real_label'])
+        # self.eval_table.to_csv(self.csv_filename, index=False)
+
+        self.gaze_data = pd.read_csv(os.path.join(self.gaze_data_dir, self.gaze_data_file_name))
+        print("The shape of gaze_data.csv: ".format(self.gaze_data.shape))
+
+        self.img_file = pd.read_csv(os.path.join(self.gaze_data_dir, self.img_file_name)) 
+        print("The shape of gaze_data.csv: ".format(self.img_file.shape))
+
+        self.prediction_file = pd.read_csv(os.path.join(self.gaze_data_dir, self.predict_file_name)) 
+        print("The shape of gaze_data.csv: ".format(self.prediction_file.shape))
+
+
+    def __del__(self):
+        if os.path.exists(self.csv_filename):
+            os.remove(self.csv_filename)
+
+        if os.path.exists(self.gaze_data_file_name):
+            os.remove(self.gaze_data_file_name)
+
+        if os.path.exists(self.img_file_name):
+            os.remove(self.img_file_name)
+
+        if os.path.exists(self.predict_file_name):
+            os.remove(self.predict_file_name)
+
+
+    def eval_res(self, delay_threshold=0.01):  
+        # self.delay_threshold = delay_threshold 
+        i = 0
+        j = 0 
+        cnt = 0
+        # df_eval = pd.read_csv(self.csv_filename)
+        # df_predict = pd.read_csv(self.predict_dir)
+
+        while i < self.img_file.shape[0] and j < self.gaze_data.shape[0]:
+            img = self.img_file.iloc[i]
+            gaze_record = self.gaze_data.iloc[j]
+            
+            gaze_point_time = gaze_record['timestamp']
+            img_time = img['time']
+            # skip time == null 
+            if gaze_point_time != gaze_point_time:
+                j += 1
+                continue
+                
+            diff_time = gaze_point_time - img_time
+            # print(i, j, diff_time)
+            if abs(diff_time) < delay_threshold:
+                print(i, j, diff_time)
+
+                # if gaze_record['FPOGY'] is None: continue
+                if gaze_record['FPOGX'] is None:
+                    j += 1
+                    continue
+                elif gaze_record['FPOGX'] < 0.5:
+                    self.eval_table.loc[cnt] = [img['img_name'], img_time, gaze_point_time, 0] # left
+                else:
+                    self.eval_table.loc[cnt] = [img['img_name'], img_time, gaze_point_time, 1] # right
+
+                cnt += 1
+                i += 1
+
+            if gaze_point_time < img_time:
+                j += 1
+            else:
+                i += 1
+
+        # res = pd.merge(df_eval, df_predict, on='img_name')  
+        self.eval_table.merge(self.prediction_file, on='img_name')
+        # df_eval.to_csv(self.csv_filename, index=False)
+        # return self.eval_table
+
+
+    def eval_plot(self):
+        # self.eval_table
+        # self.eval_table.plot(x='img_time', y=['real_label', 'predict_label'], figsize=(10,5), grid=True)
+        plt.plot(self.eval_table['img_time'], self.eval_table['real_label'], 'b-', label="real_label")
+        plt.plot(self.eval_table['img_time'], self.eval_table['predict_label'], 'r-', label="predict_label")
+        plt.legend(loc='best')
+        plt.savefig(os.path.join(os.getcwd(), 'evaluation.img'))
+        plt.show()
+
+
+if __name__ == "__main__":
+    pass
